279d789d461d50955268b230d5f87c30f264922c
Vishal_Sharma <94846078+V4Ual@users.noreply.github.com>
2025-07-11 18:27:43 +0530

    first commit

---
+++ demo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+using namespace std;
+ class myclass{
+  public:
+    int a;
+    string b;
+};
+int main(){
+  myclass myobej;
+  myobej.a =15;
+  myobej.b = "hello" ;
+
+  cout << myobej.a<<"\n";
+  cout<< myobej.b;
+   return 0;
+   }
+ 
+
+
---
+++ demo.py
@@ -0,0 +1,57 @@
+"""x="learning"
+y="is"
+z="fun"
+
+print(x+y+z)"""
+
+"""x="shani banshiala"
+
+def  demofunc():
+    x="vishal banshilal"
+    print ("suthar",x)
+demofunc()   
+
+print("bca sy" +x)
+
+"""
+"""x= float(1)
+
+y= int(2.9)
+
+z=complex(1j)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)
+print(z)"""
+"""import random
+print(random.randrange(1,50))"""
+""""x=int(1)
+y=int(2.8)
+z=int("3")
+print(z)
+print(y)
+print(z)"""
+"""x = "Banana"
+print(len(x))
+""""""
+txt ="hello,world"
+print(txt[-5:-2])"""
+"""a ="HI SHANI"
+b=a.split(',')
+print(b)"""
+"""a="hello"
+b="world"
+c=a+" "+b
+print(c)
+"""
+"""age = 36
+txt = f"my name is is shani, i am {age}"
+print(txt)"""
+
+price = 89
+txt =f"the price is {20*59} dollars"
+print(txt)
